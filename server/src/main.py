fea21150797aa1a28e355741e9ca95d9ebf8206b
최원준 <wonjundero@naver.com>
2021-01-04 21:15:39 +0900

    :zap: 서버코드 변경, create_app 사용

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+    @app.route('/')
+    def hello():
+        return "Hello World!"
+
+    return app
