3c16e09f27cb5a0909e1cbb1f01c8fa144108068
최원준 <wonjundero@naver.com>
2021-01-06 00:04:31 +0900

    :zap: 라우트 분리 성공

--- server/src/main.py
+++ server/src/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify, current_app
+from flask_restx import Api, Resource
 from sqlalchemy import create_engine, text
+from controllers.test import Test
 
 def create_app(test_config = None):		
     app = Flask(__name__)
-
-    # app.json_encoder = CustomJSONEncoder
+    api = Api(app)
 
     if test_config is None:	
         app.config.from_pyfile("./db/config.py")
@@ -13,19 +14,7 @@ def create_app(test_config = None):
 
     database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
     app.database = database	
-    
-    @app.route('/test', methods=['GET'])
-    def test():
-        row = current_app.database.execute(text("""
-            select * from log where habbit_id in (select id from habbit where userid = 1);
-        """)).fetchone()
 
-        return jsonify({
-          'id': row['id'], 
-          'habbit_id': row['habbit_id'],
-          'date': row['date'],
-          'description': row['description'],
-          'status': row['status'],
-        })
+    api.add_namespace(Test, '/test')
 
     return app	
