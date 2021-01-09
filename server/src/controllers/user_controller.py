631ac4b758e5ce4c65308f04560983891bf95a3a
wonjun0120 <wonjundero@gmail.com>
2021-01-09 12:57:21 +0900

    :zap: 회원가입 기능 추가

--- server/src/controllers/user_controller.py
+++ server/src/controllers/user_controller.py
@@ -11,8 +11,16 @@ user_login_fields = User.model('User', {  # Model 객체 생성
     'androidId': fields.String(description='안드로이드 아이디', required=True, example="123123")
 })
 
+user_signup_fields = User.model('User', {  # Model 객체 생성
+    'androidId': fields.String(description='안드로이드 아이디', required=True, example="123123"),
+    'username': fields.String(description='유저 닉네임', required=True, example="원준대로")
+})
+
 def abort_account_doesnt_exist(account_id):
     abort(404, "Account {} doesn't exist".format(account_id))
+
+def abort_account_already_exist(account_id):
+    abort(409, "Account {} already exist".format(account_id))
 
 @User.route('/login')
 class Login(Resource):
@@ -35,3 +43,32 @@ class Login(Resource):
             'id': row['id']
         }, 200
 
+@User.route('/signup')
+class Signup(Resource):
+    @User.response(201, 'Success')
+    @User.response(409, '이미 존재하는 유저입니다.')
+    @User.expect(user_signup_fields)
+    def post(self):
+        """ androidId를 바탕으로 회원가입을 진행합니다. """ 
+        androidId = request.json.get('androidId')
+        username = request.json.get('username')
+        
+        #이미 등록된 안드로이드 기기가 있는경우, (제약조건 추가?)
+        isExist = current_app.database.execute(text(f"""
+            select * from users where android_id = {androidId};
+        """)).fetchone()
+
+        if isExist is not None:
+            abort_account_already_exist(androidId)
+
+        current_app.database.execute(text(f"""
+            insert into users (android_id, user_name) value ("{androidId}", "{username}");
+        """))
+
+        row = current_app.database.execute(text(f"""
+            select * from users where android_id = {androidId};
+        """)).fetchone()
+        print(row)
+        return {
+            'id': row['id']
+        }, 201
